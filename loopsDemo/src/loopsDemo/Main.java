901a018e9671899ce26c81383f1504a5fb1bea3b
pinar.firat@etiya.com <pinar.firat@etiya.com>
2022-09-21 18:22:54 +0300

    loopcommit

---
+++ loopsDemo/src/loopsDemo/Main.java
@@ -0,0 +1,27 @@
+package loopsDemo;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// For
+	
+		
+		for (int i=2; i<=10; i+=2) {
+			System.out.print(i+", "); }
+			{ 
+			System.out.println(" For Döngüsü Bitti" );}
+		
+
+
+// While
+ 
+	int i =1;
+	
+	while (i<10 ) {
+		System.out.print(i +", ");
+		i+=2;
+		
+	}
+	System.out.println("While Döngüsü Bitti");
+}
+}
