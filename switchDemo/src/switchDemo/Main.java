04173f391dfc72c42b9f62754fffbb2f1006c544
pinar.firat@etiya.com <pinar.firat@etiya.com>
2022-09-21 18:05:09 +0300

    switchcommit

---
+++ switchDemo/src/switchDemo/Main.java
@@ -0,0 +1,35 @@
+package switchDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'B';
+
+		switch (grade) {
+		case 'A' :
+			System.out.println("Mükemmel : Geçtiniz"); 
+			break;
+			
+		case 'B' :
+			System.out.println("Çok iyi : Geçtiniz");
+			break;
+
+		case 'C' :
+			System.out.println("Orta : Geçtiniz");
+			break;
+
+		case 'D' :
+			System.out.println("Kötü : Geçtiniz");
+			break;
+
+		case 'F' :
+			System.out.println("Çok Kötü : Geçtiniz");
+			break;
+
+		default: System.out.println("Geçersiz not girdiniz");
+		break;
+			
+		}
+	}
+
+}
